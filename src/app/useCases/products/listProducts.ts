19a08b23f2c00c0462e670dd528ce7e6c1044aed
Marii <124800098+marianacostalima@users.noreply.github.com>
2023-09-26 22:26:34 -0300

    Update listProducts.ts

--- src/app/useCases/products/listProducts.ts
+++ src/app/useCases/products/listProducts.ts
@@ -1,14 +1,20 @@
+// Importa os tipos 'Request' e 'Response' do Express para lidar com solicitações e respostas HTTP.
 import { Request, Response } from 'express';
+
+// Importa o modelo 'Product' para interagir com os produtos no banco de dados.
 import { Product } from '../../models/Product';
 
+// Função assíncrona que lida com a obtenção da lista de produtos.
 export async function listProducts(req: Request, res: Response) {
-	try {
-		const products = await Product.find();
+    try {
+        // Obtém a lista de produtos do banco de dados usando o método 'find'.
+        const products = await Product.find();
 
-		res.json(products);
-	} catch (error) {
-		console.log(error);
-		res.sendStatus(500);
-	}
-
+        // Retorna a lista de produtos em formato JSON como resposta.
+        res.json(products);
+    } catch (error) {
+        // Em caso de erro, registra o erro no console e envia uma resposta com status 500 (Internal Server Error).
+        console.log(error);
+        res.sendStatus(500);
+    }
 }
