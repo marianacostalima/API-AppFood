928b523351ea8eed4bdbcb40ce80f5270d0e73b8
Marii <124800098+marianacostalima@users.noreply.github.com>
2023-09-26 22:24:20 -0300

    Update createOrder.ts

--- src/app/useCases/orders/createOrder.ts
+++ src/app/useCases/orders/createOrder.ts
@@ -1,14 +1,23 @@
+// Importa os tipos 'Request' e 'Response' do Express para lidar com solicitações e respostas HTTP.
 import { Request, Response } from 'express';
+
+// Importa o modelo 'Order' para interagir com os pedidos no banco de dados.
 import { Order } from '../../models/Order';
 
+// Função assíncrona que lida com a criação de um novo pedido.
 export async function createOrder(req: Request, res: Response) {
-	try {
-		const {table, products} = req.body;
+    try {
+        // Obtém os dados do pedido do corpo da solicitação (req.body).
+        const { table, products } = req.body;
 
-		const order = await Order.create({ table, products });
-		res.status(201).json(order);
-	} catch (error) {
-		console.log(error);
-		res.sendStatus(500);
-	}
+        // Cria uma nova instância de pedido usando o modelo 'Order' e os dados recebidos.
+        const order = await Order.create({ table, products });
+
+        // Retorna uma resposta com status 201 (Created) e o pedido criado em formato JSON.
+        res.status(201).json(order);
+    } catch (error) {
+        // Em caso de erro, registra o erro no console e envia uma resposta com status 500 (Internal Server Error).
+        console.log(error);
+        res.sendStatus(500);
+    }
 }
