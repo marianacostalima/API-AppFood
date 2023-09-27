ffe4ae008b9f1f9bf64c9d4fbd0f9c9702a83c1e
Marii <124800098+marianacostalima@users.noreply.github.com>
2023-09-26 22:20:45 -0300

    Update createCategory.ts

--- src/app/useCases/categories/createCategory.ts
+++ src/app/useCases/categories/createCategory.ts
@@ -1,14 +1,23 @@
+// Importa os tipos 'Request' e 'Response' do Express para lidar com solicitações e respostas HTTP.
 import { Request, Response } from 'express';
+
+// Importa o modelo 'Category' para interagir com as categorias no banco de dados.
 import { Category } from '../../models/Category';
 
+// Função assíncrona que lida com a criação de uma nova categoria.
 export async function createCategory(req: Request, res: Response) {
-	try {
-		//res.send('Ok post category');
-		const {icon, name} = req.body;
-		const category = await Category.create({icon, name});
-		res.status(201).json(category);
-	} catch (error) {
-		console.log(error);
-		res.sendStatus(500);
-	}
+    try {
+        // Obtém os dados da categoria do corpo da solicitação.
+        const { icon, name } = req.body;
+
+        // Cria uma nova instância de categoria usando o modelo 'Category' e os dados recebidos.
+        const category = await Category.create({ icon, name });
+
+        // Retorna uma resposta com status e a categoria criada em formato JSON.
+        res.status(201).json(category);
+    } catch (error) {
+        // Em caso de erro, registra o erro no console e envia uma resposta com status 500 (Internal Server Error).
+        console.log(error);
+        res.sendStatus(500);
+    }
 }
