072c44b101fff62ca6f57c9a79662a7036a71431
Marii <124800098+marianacostalima@users.noreply.github.com>
2023-09-26 22:21:31 -0300

    Update listCategories.ts

--- src/app/useCases/categories/listCategories.ts
+++ src/app/useCases/categories/listCategories.ts
@@ -1,13 +1,20 @@
+// Importa os tipos 'Request' e 'Response' do Express para lidar com solicitações e respostas HTTP.
 import { Request, Response } from 'express';
+
+// Importa o modelo 'Category' para interagir com as categorias no banco de dados.
 import { Category } from '../../models/Category';
 
+// Função assíncrona que lida com a obtenção da lista de categorias.
 export async function listCategories(req: Request, res: Response) {
-	try {
-		const categories = await Category.find();
+    try {
+        // Obtém a lista de categorias do banco de dados usando o método 'find'.
+        const categories = await Category.find();
 
-		res.json(categories);
-	} catch (error) {
-		console.log(error);
-		res.sendStatus(500);
-	}
+        // Retorna a lista de categorias em formato JSON como resposta.
+        res.json(categories);
+    } catch (error) {
+        // Em caso de erro, registra o erro no console e envia uma resposta com status 500 (Internal Server Error).
+        console.log(error);
+        res.sendStatus(500);
+    }
 }
