126565994101c896c9613a5f6e3241bb70e49ff2
Marii <124800098+marianacostalima@users.noreply.github.com>
2023-09-26 22:23:52 -0300

    Update changeOrderStatus.ts

--- src/app/useCases/orders/changeOrderStatus.ts
+++ src/app/useCases/orders/changeOrderStatus.ts
@@ -1,20 +1,33 @@
+// Importa os tipos 'Request' e 'Response' do Express para lidar com solicitações e respostas HTTP.
 import { Request, Response } from 'express';
+
+// Importa o modelo 'Order' para interagir com os pedidos no banco de dados.
 import { Order } from '../../models/Order';
 
+// Função assíncrona que lida com a alteração do status de um pedido.
 export async function changeOrderStatus(req: Request, res: Response) {
-	try {
-		const { orderId } = req.params;
-		const { status } = req.body;
+    try {
+        // Obtém o parâmetro 'orderId' da URL da solicitação.
+        const { orderId } = req.params;
 
-		if (!['WAITING', 'IN_PRODUCTION', 'DONE'].includes(status)){
-			return res.status(400).json({
-				error: 'Status should be one of these: WAITING, IN_PRODUCTION, DONE'
-			});
-		}
-		await Order.findByIdAndUpdate(orderId, { status });
-		res.sendStatus(204);
-	} catch (error) {
-		console.log(error);
-		res.sendStatus(500);
-	}
+        // Obtém o novo status do pedido do corpo da solicitação (req.body).
+        const { status } = req.body;
+
+        // Verifica se o novo status está entre as opções permitidas ('WAITING', 'IN_PRODUCTION', 'DONE').
+        if (!['WAITING', 'IN_PRODUCTION', 'DONE'].includes(status)) {
+            return res.status(400).json({
+                error: 'Status should be one of these: WAITING, IN_PRODUCTION, DONE'
+            });
+        }
+
+        // Usa o método 'findByIdAndUpdate' do modelo 'Order' para encontrar o pedido pelo ID e atualizar seu status.
+        await Order.findByIdAndUpdate(orderId, { status });
+
+        // Retorna uma resposta com status 204 (No Content) para indicar que o status do pedido foi alterado com sucesso.
+        res.sendStatus(204);
+    } catch (error) {
+        // Em caso de erro, registra o erro no console e envia uma resposta com status 500 (Internal Server Error).
+        console.log(error);
+        res.sendStatus(500);
+    }
 }
