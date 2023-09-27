2ed1546a3b40078d9b65a9e3ee0ab99ff8ebf06a
Marii <124800098+marianacostalima@users.noreply.github.com>
2023-09-26 22:12:46 -0300

    Update Category.ts

--- src/app/models/Category.ts
+++ src/app/models/Category.ts
@@ -1,12 +1,14 @@
+//criar um modelo de dados.
 import {model, Schema } from 'mongoose';
-
+// Define o modelo de dados 'Category'
 export const Category = model('Category', new Schema({
+	// Define os campos da categoria.
 	name: {
-		type: String,
-		required: true,
+		type: String,     // O tipo do campo string.
+		required: true,   // É obrigatório ter um valor para esse campo.
 	},
 	icon: {
-		type: String,
-		required: true,
+		type: String,     // O tipo do campo string.
+		required: true,   // É obrigatório ter um valor para esse campo.
 	}
 }));
